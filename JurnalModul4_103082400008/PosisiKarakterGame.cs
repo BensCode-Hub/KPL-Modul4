913a23542a3ad6861575e89ea297a64b6f793d5a
BensCode <ruben.simarmata2@gmail.com>
2026-03-18 05:23:44 +0700

    commit kedua

---
+++ JurnalModul4_103082400008/PosisiKarakterGame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JurnalModul4_103082400008
+{
+    internal class PosisiKarakterGame
+    {
+       
+    public enum State { Jongkok, Berdiri, Tengkurap, Terbang }
+
+        private State currentState = State.Berdiri;
+
+        public void TekanTombol(string tombol,int  nim)
+        {
+            State nextState = currentState;
+            int hasilMod =  nim % 3;
+
+            if (hasilMod == 1)
+            {
+                if (currentState != State.Berdiri && nextState == State.Berdiri) 
+                    Console.WriteLine("posisi standby");
+                if (currentState != State.Tengkurap && nextState == State.Tengkurap) 
+                    Console.WriteLine("posisi istirahat");
+            }
+
+            if (currentState == State.Berdiri)
+            {
+                if (tombol == "TombolS") nextState = State.Jongkok;
+                if (tombol == "TombolW") nextState = State.Terbang;
+            }
+            else if (currentState == State.Jongkok)
+            {
+                if (tombol == "TombolW") nextState = State.Berdiri;
+                if (tombol == "TombolS") nextState = State.Tengkurap;
+            }
+            else if (currentState == State.Tengkurap)
+            {
+                if (tombol == "TombolW") nextState = State.Jongkok;
+            }
+            else if (currentState == State.Terbang)
+            {
+                if (tombol == "TombolS") nextState = State.Berdiri;
+                if (tombol == "TombolX") nextState = State.Jongkok;
+            }
+
+            currentState = nextState;
+            Console.WriteLine("State sekarang: " + currentState);
+        }
+    }
+}
+    
+
+    
+
--- JurnalModul4_103082400008/Program.cs
+++ JurnalModul4_103082400008/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+namespace JurnalModul4_103082400008
+{
+    public class Program
+    {
+        static void Main(string[] args)
+    
+        {
+            kodeBuah kodeBuah = new kodeBuah();
+
+            Console.Write("Masukkan nama buah: ");
+
+            string namaBuah = Console.ReadLine();
+
+            string hasilKode = kodeBuah.getkodeBuah(namaBuah);
+
+            Console.WriteLine("Kode buah " + namaBuah + " adalah: " + hasilKode);
+
+
+            Console.WriteLine("\nPosisi Kareakter Game\n");
+
+            int nim = 1030824008;
+            PosisiKarakterGame pemain = new PosisiKarakterGame();
+
+            Console.WriteLine(" Simulasi Perubahan State: ");
+
+            pemain.TekanTombol("Tombol W", nim);
+            pemain.TekanTombol("Tombol X", nim);
+            pemain.TekanTombol("Tombol S", nim);
+            pemain.TekanTombol("Tombol W", nim);
+        }
+    }
+}
